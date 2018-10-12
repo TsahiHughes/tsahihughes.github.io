63794cd9f3fb8652ccbc42305df54c5baace0f7a
TsahiHughes <42795459+TsahiHughes@users.noreply.github.com>
2018-10-11 17:00:20 -0700

    created css

---
+++ main.css
@@ -0,0 +1,11 @@
+body {
+  margin: 30px auto;
+  max-width: 1000px;
+  font-family: "Helvetica", "Arial", sans-serif;
+  line-height: 1.5;
+  padding: 30px 20px;
+}
+
+h1 {
+  padding: 20px 0px 10px 0px;
+}
